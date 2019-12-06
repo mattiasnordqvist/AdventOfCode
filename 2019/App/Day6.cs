6e045d814a2e32dc4b33ae0d6ff1fc656ef4810f
Mattias Nordqvist <mattias.nordqvist@gmail.com>
2019-12-06 10:47:42 +0100

    Create Day6.cs

---
+++ 2019/App/Day6.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace App
+{
+    public class Day6 : Day
+    {
+
+        protected override string Part1Code(string data)
+        {
+
+            var orbits = data.Lines().Select(ParseOrbit).ToDictionary(x => x.Item2);
+            return orbits.Values.Sum(x => BuildPathToCOM(x, orbits).Count()).ToString();
+        }
+
+        protected override string Part2Code(string data)
+        {
+            var orbits = data.Lines().Select(ParseOrbit).ToDictionary(x => x.Item2);
+            List<string> santasPath = BuildPathToCOM(orbits["SAN"], orbits);
+            List<string> yousPath = BuildPathToCOM(orbits["YOU"], orbits);
+            int commonOrbitsCount = 0;
+            while (true)
+            {
+                if (santasPath[commonOrbitsCount] != yousPath[commonOrbitsCount])
+                {
+                    break;
+                }
+                commonOrbitsCount++;
+            }
+
+            return (santasPath.Count() + yousPath.Count() - commonOrbitsCount * 2).ToString();
+        }
+
+        private static (string, string) ParseOrbit(string x)
+        {
+            return (x.Substring(0, x.IndexOf(")")), x.Substring(x.IndexOf(")") + 1));
+        }
+
+        private static List<string> BuildPathToCOM((string, string) p, Dictionary<string,(string,string)> orbits)
+        {
+            // no need to cache. Problem is pretty small.
+            var list = new LinkedList<string>();
+            list.AddFirst(p.Item1);
+            while (p.Item1 != "COM")
+            {
+                p = orbits[p.Item1];
+                list.AddFirst(p.Item1);
+            }
+            return new List<string>(list);
+        }
+    }
+}
