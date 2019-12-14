ce4537f219dbc92d6a078d15e25a30d73164fd30
Mattias Nordqvist <mattias.nordqvist@gmail.com>
2019-12-14 12:24:16 +0100

    Update Day14.cs
    internet suggested binary search. of course. *blush*

--- 2019/App/Day14.cs
+++ 2019/App/Day14.cs
@@ -66,9 +66,6 @@ namespace App
 
         protected override string Part2Code(string data)
         {
-            var restProducts = new Dictionary<string, long>();
-            restProducts["ORE"] = 1000000000000;
-
             var reactions = new Dictionary<(long n, string r), List<(long n, string r)>>();
             foreach (var line in data.Lines())
             {
@@ -78,67 +75,71 @@ namespace App
                 var input = left.Split(", ").Select(x => x.Trim()).Select(x => (long.Parse(x.Split(" ")[0].Trim()), x.Split(" ")[1].Trim())).ToList();
                 reactions[output] = input;
             }
-            var any = reactions.Select(x => x.Key).GroupBy(x => x.r).Any(x => x.Count() > 1);
-            if (any)
-            {
-                throw new Exception("unexpected duplicate output paths");
-            }
-            var fuelProduced = 0;
+
+
+
+            long min = 1;
+            long max = -1;
             while (true)
             {
+
+                long requiredFuel = max < 0 ? min * 2 : ((max - min) / 2) + min;
+
+                var restProducts = new Dictionary<string, long>();
                 var requiredResources = new Dictionary<string, long>();
-                requiredResources["FUEL"] = 1;
-                while (requiredResources.Any(x => x.Value > 0))
+                requiredResources["FUEL"] = requiredFuel;
+                while (requiredResources.Any(x => x.Key != "ORE" && x.Value > 0))
                 {
-                    var satisfyMe = requiredResources.Where(x => x.Value > 0).First();
+                    var satisfyMe = requiredResources.Where(x => x.Key != "ORE" && x.Value > 0).First();
                     var amountNeeded = satisfyMe.Value;
 
-                    if (restProducts.ContainsKey(satisfyMe.Key) && restProducts[satisfyMe.Key] > 0)
+                    if (restProducts.ContainsKey(satisfyMe.Key))
                     {
-                        if (satisfyMe.Key == "ORE" && restProducts["ORE"] < satisfyMe.Value)
-                        {
-                            return fuelProduced.ToString();
-                        }
                         var usedRestProductsAmount = Math.Min(amountNeeded, restProducts[satisfyMe.Key]);
                         amountNeeded -= usedRestProductsAmount;
                         Add(restProducts, satisfyMe.Key, -usedRestProductsAmount);
                     }
-                    if (satisfyMe.Key != "ORE")
-                    {
-                        var reaction = reactions.Single(x => x.Key.r == satisfyMe.Key);
-                        if (amountNeeded > 0)
-                        {
-                            var reactionsNeeded = amountNeeded / reaction.Key.n;
-                            if (amountNeeded % reaction.Key.n != 0)
-                            {
-                                reactionsNeeded++;
-                            }
-                            foreach (var input in reaction.Value)
-                            {
-                                Add(requiredResources, input.r, input.n * reactionsNeeded);
-                            }
-                            amountNeeded -= (reaction.Key.n * reactionsNeeded);
-                        }
-                        if (amountNeeded < 0)
-                        {
-                            Add(restProducts, reaction.Key.r, -amountNeeded);
-                        }
 
-                    }
-                    if (amountNeeded > 0)
+                    var reaction = reactions.Single(x => x.Key.r == satisfyMe.Key);
+                    while (amountNeeded > 0)
                     {
-                        throw new Exception("unexpected");
+                        var reactionsNeeded = amountNeeded / reaction.Key.n;
+                        if (amountNeeded % reaction.Key.n != 0)
+                        {
+                            reactionsNeeded++;
+                        }
+                        foreach (var input in reaction.Value)
+                        {
+                            Add(requiredResources, input.r, input.n * reactionsNeeded);
+                        }
+                        amountNeeded -= (reaction.Key.n * reactionsNeeded);
                     }
-                    requiredResources.Remove(satisfyMe.Key);
+                    Add(restProducts, reaction.Key.r, -amountNeeded);
+                    requiredResources[satisfyMe.Key] = 0;
                 }
-                fuelProduced++;
-                if (fuelProduced % 10000 == 0)
+                var oreCost = requiredResources.First(x => x.Key == "ORE").Value;
+                Console.WriteLine(requiredFuel + " fuel costs " + oreCost + " ore.");
+                if (max < 0 && oreCost < 1000000000000)
                 {
-                    Console.WriteLine("Fuel produced: " + fuelProduced);
-                    Console.WriteLine("Ore left: " + restProducts["ORE"]);
+                    min = min * 2;
+
+                }
+                else if (oreCost < 1000000000000)
+                {
+                    min = requiredFuel;
+                }
+                else if (oreCost > 1000000000000)
+                {
+                    max = requiredFuel;
+                }
+                else
+                {
+                    //check console output. don't know exactly when to break here :D
+                    return requiredFuel.ToString();
+
                 }
             }
-            throw new Exception("unexpected");
+
         }
     }
 }
